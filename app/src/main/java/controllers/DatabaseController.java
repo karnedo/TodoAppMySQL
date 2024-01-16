de9972ab8e44e930ff9f6aa6ce005329b527480e
karnedo <karnedo@proton.me>
2024-01-16 14:36:27 +0100

    Revert "Added new translations to DatabaseController"
    This reverts commit e00034c39e3feefbdffdb0d2f95b0b56f859b275.

--- app/src/main/java/controllers/DatabaseController.java
+++ app/src/main/java/controllers/DatabaseController.java
@@ -1,15 +1,28 @@
 package controllers;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Base64;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 import com.example.todoapp.LoginActivtity;
 import com.example.todoapp.R;
 import com.example.todoapp.TaskListActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageException;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -26,6 +39,7 @@ import org.json.JSONObject;
 import java.io.ByteArrayOutputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -34,21 +48,21 @@ import models.Task;
 import models.ProviderType;
 
 public class DatabaseController {
-    private static final String DATABASE_URL = "http://192.168.1.71/";
-    private static final String PREPROCESSOR_URL = DATABASE_URL + "tasks";
-    private static final String INSERT_TASK = "/insert_.php";
-    private static final String GET_TASKS = "/get_tasks_.php";
-    private static final String DELETE_TASK = "/delete_task.ph";
-    private static final String CHECK_TASK = "/check_task_.php";
-    private static final String SIGN_UP = "/sign_up_.php";
-    private static final String LOG_IN = "/log_in_.php";
-    private static final String INSERT_PICTURE = "/insert_picture_.php";
-    private static final String GET_PICTURE = "/get_picture_.ph";
 
+    public static final String DATABASE_URL = "http://192.168.1.71/";
+    public static final String PREPROCESSOR_URL = DATABASE_URL + "tasks";
+
+    private FirebaseDatabase database;
+    private DatabaseReference ref;
+    private ArrayList<Task> tasks;
     private TaskListActivity taskListActivity;
     private Context context;
 
     public DatabaseController(TaskListActivity taskListActivity, Context context){
+        database = FirebaseDatabase.getInstance("https://to-do-app-9be18-default-rtdb.europe-west1.firebasedatabase.app/");
+        //ref = database.getReference("tasks");
+        ref = database.getReference("" + FirebaseAuth.getInstance().getUid() + "/tasks");
+        tasks = new ArrayList<Task>(0);
         this.taskListActivity = taskListActivity;
         this.context = context;
     }
@@ -56,23 +70,21 @@ public class DatabaseController {
     /***************************TASKS***************************/
 
     public void saveTask(Task task, String email){
-        StringRequest request =new StringRequest(Request.Method.POST, PREPROCESSOR_URL + INSERT_TASK,
+        StringRequest request =new StringRequest(Request.Method.POST, PREPROCESSOR_URL + "/insert_.php",
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        Log.i("MyApp", response);
                         if (response.trim().equalsIgnoreCase("success")) {
+                            Log.i("MyApp", "Success");
                         } else {
-                            Toast.makeText(context,
-                                    context.getResources().getString(R.string.Error_save_task),
-                                    Toast.LENGTH_SHORT).show();
+                            Log.i("MyApp", "Fail onResponse on save task");
                         }
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(context,
-                        context.getResources().getString(R.string.Error_save_task),
-                        Toast.LENGTH_SHORT).show();
+                Log.i("MyApp", "Fail onErrorResponse: " + error.getMessage() + " , " + error.getCause());
             }
         }
         ){
@@ -92,20 +104,22 @@ public class DatabaseController {
             }
         };
         RequestQueue requestQueue = Volley.newRequestQueue(context);
+        Log.i("MyApp", "Request: " + request);
         requestQueue.add(request);
     }
 
     public void loadTasks(String email) {
-        StringRequest request =new StringRequest(Request.Method.POST, PREPROCESSOR_URL+ GET_TASKS,
+        StringRequest request =new StringRequest(Request.Method.POST, PREPROCESSOR_URL+ "/get_tasks_.php",
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         try {
+                            Log.i("MyApp", "Response: " + response);
                             JSONObject jsonObject = new JSONObject(response);
-                            String success=jsonObject.getString("success");
+                            String exito=jsonObject.getString("success");
                             JSONArray jsonArray =jsonObject.getJSONArray("kraTASKS");
 
-                            if (success.equals("1")){
+                            if (exito.equals("1")){
                                 for (int i=0;i<jsonArray.length();i++) {
                                     JSONObject object = jsonArray.getJSONObject(i);
                                     int id = object.getInt("idTask");
@@ -135,15 +149,14 @@ public class DatabaseController {
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(context,
-                        context.getResources().getString(R.string.Error_load_task),
-                        Toast.LENGTH_SHORT).show();
+                Toast.makeText(context,error.getMessage(),Toast.LENGTH_SHORT).show();
             }
         }
         ){
             @Override
             protected Map<String, String> getParams() {
                 Map<String,String>params=new HashMap<>();
+                Log.i("MyApp", "Email: " + email);
                 params.put("email", email);
                 return params;
             }
@@ -154,24 +167,21 @@ public class DatabaseController {
     }
 
     public void deleteTask(Context context, Task task){
-        StringRequest request =new StringRequest(Request.Method.POST, PREPROCESSOR_URL + DELETE_TASK,
+        StringRequest request =new StringRequest(Request.Method.POST, PREPROCESSOR_URL + "/delete_task.php",
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         //Use of trim because somehow response has invisible characters
                         if (response.trim().equalsIgnoreCase("success")) {
+                            Log.i("MyApp", "Success");
                         } else {
-                            Toast.makeText(context,
-                                    context.getResources().getString(R.string.Error_delete_task),
-                                    Toast.LENGTH_SHORT).show();
+                            Log.i("MyApp", "Fail: " + response);
                         }
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(context,
-                        context.getResources().getString(R.string.Error_delete_task),
-                        Toast.LENGTH_SHORT).show();
+                Log.i("MyApp", "onErrorResponse");
             }
         }
         ){
@@ -187,24 +197,21 @@ public class DatabaseController {
     }
 
     public void checkTask(Context context, Task task, boolean isChecked){
-        StringRequest request =new StringRequest(Request.Method.POST, PREPROCESSOR_URL + CHECK_TASK,
+        StringRequest request =new StringRequest(Request.Method.POST, PREPROCESSOR_URL + "/check_task_.php",
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         //Use of trim because somehow response has invisible characters
                         if (response.trim().equalsIgnoreCase("success")) {
+                            Log.i("MyApp", "Updated task");
                         } else {
-                            Toast.makeText(context,
-                                    context.getResources().getString(R.string.Error_check_task),
-                                    Toast.LENGTH_SHORT).show();
+                            Log.i("MyApp", "Couldnt update task");
                         }
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(context,
-                        context.getResources().getString(R.string.Error_check_task),
-                        Toast.LENGTH_SHORT).show();
+                Log.i("MyApp", "Update task onErrorResponse");
             }
         }
         ){
@@ -223,24 +230,20 @@ public class DatabaseController {
     /***************************USERS***************************/
 
     public void signUp(String email, String password, LoginActivtity loginScreen){
-        StringRequest request =new StringRequest(Request.Method.POST, PREPROCESSOR_URL + SIGN_UP,
+        StringRequest request =new StringRequest(Request.Method.POST, PREPROCESSOR_URL + "/sign_up_.php",
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         if (response.trim().equalsIgnoreCase("success")) {
                             loginScreen.logIn(ProviderType.EMAIL, email);
                         } else {
-                            Toast.makeText(context,
-                                    context.getResources().getString(R.string.Error_SignUp),
-                                    Toast.LENGTH_SHORT).show();
+                            loginScreen.showError("No ha sido posible el registro");
                         }
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(context,
-                        context.getResources().getString(R.string.Error_SignUp),
-                        Toast.LENGTH_SHORT).show();
+                loginScreen.showError("No ha sido posible el registro");
             }
         }
         ){
@@ -257,20 +260,21 @@ public class DatabaseController {
     }
 
     public void logIn(String email, String password, LoginActivtity loginScreen){
-        StringRequest request =new StringRequest(Request.Method.POST, PREPROCESSOR_URL + LOG_IN,
+        StringRequest request =new StringRequest(Request.Method.POST, PREPROCESSOR_URL + "/log_in_.php",
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        Log.i("MyApp", response);
                         if (response.trim().equalsIgnoreCase("success")) {
                             loginScreen.logIn(ProviderType.EMAIL, email);
                         } else {
-                            loginScreen.showError(context.getResources().getString(R.string.Error_LogIn));
+                            loginScreen.showError("No ha sido posible iniciar sesión");
                         }
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                loginScreen.showError(context.getResources().getString(R.string.Error_LogIn));
+                loginScreen.showError("No ha sido posible iniciar sesión");
             }
         }
         ){
@@ -288,23 +292,21 @@ public class DatabaseController {
 
     /***************************PROFILE PICTURE***************************/
     public void saveProfilePicture(String email, Bitmap bitmap){
-        StringRequest request =new StringRequest(Request.Method.POST, PREPROCESSOR_URL + INSERT_PICTURE,
+        StringRequest request =new StringRequest(Request.Method.POST, PREPROCESSOR_URL + "/insert_picture_.php",
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        Log.i("MyApp", "Picture: " + response);
                         if (response.trim().equalsIgnoreCase("success")) {
+                            Log.i("MyApp", "Saved picture");
                         } else {
-                            Toast.makeText(context,
-                                    context.getResources().getString(R.string.Error_save_picture),
-                                    Toast.LENGTH_SHORT).show();
+                            Log.i("MyApp", "Couldnt save picture");
                         }
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(context,
-                        context.getResources().getString(R.string.Error_save_picture),
-                        Toast.LENGTH_SHORT).show();
+                Log.i("MyApp", "Couldnt save picture: " + error.getClass() + " -> " + error.getMessage());
             }
         }
         ){
@@ -326,11 +328,12 @@ public class DatabaseController {
     }
 
     public void getProfilePicture(String email){
-        StringRequest request =new StringRequest(Request.Method.POST, PREPROCESSOR_URL+ GET_PICTURE,
+        StringRequest request =new StringRequest(Request.Method.POST, PREPROCESSOR_URL+ "/get_picture_.php",
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         try {
+                            Log.i("MyApp", "Response: " + response);
                             JSONObject jsonObject = new JSONObject(response);
                             String success=jsonObject.getString("success");
 
@@ -357,9 +360,7 @@ public class DatabaseController {
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(context,
-                        context.getResources().getString(R.string.Error_get_picture),
-                        Toast.LENGTH_SHORT).show();
+                Toast.makeText(context,error.getMessage(),Toast.LENGTH_SHORT).show();
             }
         }
         ){
@@ -375,7 +376,7 @@ public class DatabaseController {
         requestQueue.add(request);
     }
 
-    //metodo que convierte byte[] a bitmap
+    //método que convierte byte[] a bitmap
     private Bitmap bytes_to_bitmap(byte[] b, int width, int height){
         Bitmap.Config config = Bitmap.Config.ARGB_8888;
         Bitmap bitmap = Bitmap.createBitmap(width, height,config);
@@ -386,14 +387,16 @@ public class DatabaseController {
         return bitmap;
     }
 
-    //metodo que convierte bitmap a byte[]
+    //método que convierte bitmap a byte[]
     public static byte[] bitmap_to_bytes(Bitmap bitmap)
     {
-        if(bitmap == null) return null;
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG, 50, bos);
-        byte[] bArray = bos.toByteArray();
-        return bArray;
+        if(bitmap != null){
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 50, bos);
+            byte[] bArray = bos.toByteArray();
+            return bArray;
+        }
+        return null;
     }
 
 }
